022899d7a145cc4b174e653cea085b555c7fd349
conorf50 <conorf50@gmail.com>
2016-04-26 18:45:26 +0100

    Updated
    Changed LCD printout to say "IP = x.x.x.x"
    Attempted to make multiple buttons.

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 from wyliodrin import *
-
 import os
-
 import struct
-
 from datetime import *
 from subprocess import *
-
 from flask import Flask
 app = Flask (__name__)
-
 from flask import render_template
-
 from flask import request
 
 
 
-cmd = "ip addr show enp0s20f6 | grep inet | awk '{print $2}' | cut -d/ -f1"
+cmd = "A=$(ip addr show dev enp0s20f6); A=${A##*inet }; echo ${A%%/*}"
 def run_cmd(cmd):
         p = Popen(cmd, shell=True, stdout=PIPE)
         output = p.communicate()[0]
@@ -47,24 +41,28 @@ print('Starting...')
 ipaddr = run_cmd(cmd)
 
 digitalWrite (13, 1)
-color = colorToRGB('#33ccff')
+color = colorToRGB('#33ccfe')
 lcd2.setRGB(color[0], color[1], color[2] )
-lcd2.write(str( ipaddr ) )
+lcd2.write("IP=" + str(ipaddr)+ "    " )
 
-@app.route ("/led", methods=['GET'])
+@app.route ("/control", methods=['GET'])
 def route():
   return render_template ('/form.html', lcd=lcd)
   
-@app.route ("/led", methods=['POST'])
+@app.route ("/control", methods=['POST'])
 def route2():
-  if (request.form['state']) == 'on':
+  if (request.form['state']) == 'relayOn':
     digitalWrite(grove+4, 1)
-  elif (request.form['state']) == 'off':
+  if (request.form['state']) == 'relayOff':
     digitalWrite(grove+4, 0)
   return render_template ('/form.html', lcd=lcd)
   
-  
-  
-  
-  
+@app.route ("/control", methods=['POST'])
+def route3():
+  if (request.form['state']) == 'on':
+    digitalWrite(grove+4, 0)
+  if (request.form['state']) == 'off':
+    digitalWrite(grove+4, 0)    
+  return render_template ('/form.html', lcd=lcd)
+ 
 app.run (host='0.0.0.0')
